44faacf0198bdca2a338bde061ea24472a35d07d
pedrorribeiro6 <pedro.r.ribeiro6@aluno.senai.br>
2026-02-13 11:53:58 -0300

    salvando e finalizando

--- game.py
+++ game.py
@@ -1,9 +1,20 @@
 import flet as ft
 
 def main(page: ft.Page):
+    mensagem = ft.Text("Escolha o refrigerante certo!")
+    resposta_correta = "Guaraná"
+    
+    def verificar_resposta(e):
+        if e.control.content == resposta_correta:
+            mensagem.value = "GUARANA"
+        else:
+            mensagem.value = "Resposta Errada"     
+        page.update()
+    
     page.title = "Game: Adivinhe a imagem"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = ft.Colors = "#C78B8B"
 
     page.add(
       ft.Column(
@@ -14,10 +25,35 @@ def main(page: ft.Page):
                   weight="bold"    
                   ),
                   ft.Image(
-                      src="images/gatinho.jpg",
+                      src="images/hornet.jpeg",
                       height=200
-                  )
-          ]
+                  ),
+                  ft.Row(
+                      controls=[
+                          ft.Button(
+                              content="Guaraná",
+                              on_click=verificar_resposta,
+                              bgcolor="#DEAFAF",
+                              color="white"
+                          ),
+                          ft.Button(
+                              content="Pepsi",
+                              on_click=verificar_resposta,
+                              bgcolor="#DEAFAF",
+                              color="white"
+                          ),
+                          ft.Button(
+                              content="Coca-Cola",
+                              on_click=verificar_resposta,
+                              bgcolor="#DEAFAF",
+                              color="white"
+                          ),
+                      ],
+                      alignment=ft.MainAxisAlignment.CENTER
+                  ),
+                  mensagem
+          ],
+          horizontal_alignment=ft.CrossAxisAlignment.CENTER
       )
     )
 
